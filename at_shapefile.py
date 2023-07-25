de281064f2749ba0fdf3129732947c1d9a6be9fd
roboes <rg.roberto@outlook.com>
2023-07-25 20:31:26 +0200

    Update

--- at_shapefile.py
+++ at_shapefile.py
@@ -1,5 +1,8 @@
 ## AT Shapefile
-# Last update: 2023-05-31
+# Last update: 2023-07-20
+
+
+"""About: Austrian shapefile creation and manipulation using GeoPandas library in Python or sf library in R."""
 
 
 ###############
@@ -22,13 +25,12 @@ from matplotlib import pyplot
 import numpy as np
 import pandas as pd
 import requests
+
 # import xlrd
 
 
 # Set working directory
 os.chdir(path=os.path.join(os.path.expanduser('~'), 'Downloads'))
-
-
 
 
 ##############
@@ -39,50 +41,112 @@ os.chdir(path=os.path.join(os.path.expanduser('~'), 'Downloads'))
 # Postlexikon - Source: Post AG, https://www.post.at/g/c/postlexikon
 
 # Get page source
-page_source = urlopen(url=Request(url='https://www.post.at/g/c/postlexikon', headers={'User-Agent': 'Mozilla'})).read().decode(encoding='utf8')
+page_source = (
+    urlopen(
+        url=Request(
+            url='https://www.post.at/g/c/postlexikon',
+            headers={'User-Agent': 'Mozilla'},
+        ),
+    )
+    .read()
+    .decode(encoding='utf8')
+)
 page_source = page_source.split(sep='\n')
 
 
 # Get latest PLZ Verzeichnis .xls file
 plz_verzeichnis = [s for s in page_source if 'title="PLZ Verzeichnis"' in s][0]
-plz_verzeichnis = re.sub(pattern=r'^.*href="(.*\.xls)?.*$', repl=r'\1', string=plz_verzeichnis)
+plz_verzeichnis = re.sub(
+    pattern=r'^.*href="(.*\.xls)?.*$',
+    repl=r'\1',
+    string=plz_verzeichnis,
+)
 
 
 # Download and import
-at_postalcodes = (pd.read_excel(io=urlopen(url=Request(url=plz_verzeichnis, headers={'User-Agent': 'Mozilla'})).read(), sheet_name='Plz_Anhang', header=0, index_col=None, skiprows=0, skipfooter=0, dtype=None, engine='xlrd')
-
+at_postalcodes = (
+    pd.read_excel(
+        io=urlopen(
+            url=Request(url=plz_verzeichnis, headers={'User-Agent': 'Mozilla'}),
+        ).read(),
+        sheet_name='Plz_Anhang',
+        header=0,
+        index_col=None,
+        skiprows=0,
+        skipfooter=0,
+        dtype=None,
+        engine='xlrd',
+    )
     # Rename columns
     .rename(columns={'PLZ': 'postal_code', 'Ort': 'city', 'Bundesland': 'state'})
-
     # Change dtypes
     .astype(dtype={'postal_code': 'str'})
-
     # Create 'country' column
-    .assign(country = 'AT')
-
+    .assign(country='AT')
     # Filter rows
     .query('adressierbar == "Ja"')
-
     # Select columns
     .filter(items=['country', 'postal_code', 'state', 'city'])
-
     # Transform columns
     .assign(
-        state = lambda row: np.where(row['state'] == 'W', 'Vienna',
-            (np.where(row['state'] == 'N', 'Lower Austria',
-            (np.where(row['state'] == 'B', 'Burgenland',
-            (np.where(row['state'] == 'O', 'Upper Austria',
-            (np.where(row['state'] == 'Sa', 'Salzburg',
-            (np.where(row['state'] == 'T', 'Tyrol',
-            (np.where(row['state'] == 'V', 'Vorarlberg',
-            (np.where(row['state'] == 'St', 'Styria',
-            (np.where(row['state'] == 'K', 'Carinthia',
-            None)))))))))))))))))
+        state=lambda row: np.where(
+            row['state'] == 'W',
+            'Vienna',
+            (
+                np.where(
+                    row['state'] == 'N',
+                    'Lower Austria',
+                    (
+                        np.where(
+                            row['state'] == 'B',
+                            'Burgenland',
+                            (
+                                np.where(
+                                    row['state'] == 'O',
+                                    'Upper Austria',
+                                    (
+                                        np.where(
+                                            row['state'] == 'Sa',
+                                            'Salzburg',
+                                            (
+                                                np.where(
+                                                    row['state'] == 'T',
+                                                    'Tyrol',
+                                                    (
+                                                        np.where(
+                                                            row['state'] == 'V',
+                                                            'Vorarlberg',
+                                                            (
+                                                                np.where(
+                                                                    row['state']
+                                                                    == 'St',
+                                                                    'Styria',
+                                                                    (
+                                                                        np.where(
+                                                                            row['state']
+                                                                            == 'K',
+                                                                            'Carinthia',
+                                                                            None,
+                                                                        )
+                                                                    ),
+                                                                )
+                                                            ),
+                                                        )
+                                                    ),
+                                                )
+                                            ),
+                                        )
+                                    ),
+                                )
+                            ),
+                        )
+                    ),
+                )
+            ),
+        ),
     )
-
     # Rearrange rows
     .sort_values(by=['country', 'postal_code'], ignore_index=True)
-
 )
 
 
@@ -90,125 +154,188 @@ at_postalcodes = (pd.read_excel(io=urlopen(url=Request(url=plz_verzeichnis, head
 del page_source, plz_verzeichnis
 
 
-
-## Austria, Municipality List sort by Identfier
+## Austria, Municipality List sort by Identifier
 # Österreichisch, Gemeindeliste sortiert nach Gemeindekennziffer - Source: Statistik Austria, https://www.statistik.at/services/tools/services/regionales/regionale-gliederungen/
 
 # Download and import
-at_municipalities = (pd.read_csv(filepath_or_buffer='https://www.statistik.at/verzeichnis/reglisten/gemliste_knz_en.csv', sep=';', header=0, index_col=None, skiprows=2, skipfooter=1, dtype=None, engine='python', encoding='utf8')
-
+at_municipalities = (
+    pd.read_csv(
+        filepath_or_buffer='https://www.statistik.at/verzeichnis/reglisten/gemliste_knz_en.csv',
+        sep=';',
+        header=0,
+        index_col=None,
+        skiprows=2,
+        skipfooter=1,
+        dtype=None,
+        engine='python',
+        encoding='utf8',
+    )
     # Rename columns
-    .rename(columns={'Municipality Name': 'municipality', 'Municipality Code': 'municipality_code', 'Postal Code of the Municipal': 'postal_code'})
-
+    .rename(
+        columns={
+            'Municipality Name': 'municipality',
+            'Municipality Code': 'municipality_code',
+            'Postal Code of the Municipal': 'postal_code',
+        },
+    )
     # Change dtypes
     .astype(dtype={'municipality_code': 'str', 'postal_code': 'str'})
-
     # Select columns
     .filter(items=['municipality_code', 'municipality', 'postal_code'])
-
     # Rearrange rows
     .sort_values(by=['municipality_code'], ignore_index=True)
-
 )
-
 
 
 ## Austria, Political Districts
 # Österreich, Politische Bezirke - Source: Statistik Austria, https://www.statistik.at/services/tools/services/regionales/regionale-gliederungen/
 
 # Download and import
-at_political_districts = (pd.read_csv(filepath_or_buffer='http://www.statistik.at/verzeichnis/reglisten/polbezirke_en.csv', sep=';', header=0, index_col=None, skiprows=2, skipfooter=1, dtype=None, engine='python', encoding='utf8')
-
+at_political_districts = (
+    pd.read_csv(
+        filepath_or_buffer='http://www.statistik.at/verzeichnis/reglisten/polbezirke_en.csv',
+        sep=';',
+        header=0,
+        index_col=None,
+        skiprows=2,
+        skipfooter=1,
+        dtype=None,
+        engine='python',
+        encoding='utf8',
+    )
     # Rename columns
-    .rename(columns={'Federal Province': 'state', 'Political District': 'political_district', 'Pol. District Code': 'political_district_code'})
-
+    .rename(
+        columns={
+            'Federal Province': 'state',
+            'Political District': 'political_district',
+            'Pol. District Code': 'political_district_code',
+        },
+    )
     # Change dtypes
     .astype(dtype={'political_district_code': 'str'})
-
     # Select columns
     .filter(items=['political_district_code', 'political_district', 'state'])
-
     # Rearrange rows
     .sort_values(by=['political_district_code'], ignore_index=True)
-
 )
-
 
 
 ## Austria, Municipalities with Localities and Postal Codes
 # Österreich, Gemeinden mit Ortschaften und Postleitzahlen - Source: Statistik Austria, https://www.statistik.at/services/tools/services/regionales/regionale-gliederungen/
 
 # Download and import
-at_localities = (pd.read_csv(filepath_or_buffer='http://www.statistik.at/verzeichnis/reglisten/ortsliste.csv', sep=';', header=0, index_col=None, skiprows=2, skipfooter=1, dtype=None, engine='python', encoding='utf8')
-
+at_localities = (
+    pd.read_csv(
+        filepath_or_buffer='http://www.statistik.at/verzeichnis/reglisten/ortsliste.csv',
+        sep=';',
+        header=0,
+        index_col=None,
+        skiprows=2,
+        skipfooter=1,
+        dtype=None,
+        engine='python',
+        encoding='utf8',
+    )
     # Rename columns
-    .rename(columns={'Gemeindekennziffer': 'gemeindekennziffer', 'Gemeindename': 'municipality', 'Ortschaftsname': 'city', 'Postleitzahl': 'postal_code'})
-
+    .rename(
+        columns={
+            'Gemeindekennziffer': 'gemeindekennziffer',
+            'Gemeindename': 'municipality',
+            'Ortschaftsname': 'city',
+            'Postleitzahl': 'postal_code',
+        },
+    )
     # Change dtypes
     .astype(dtype={'gemeindekennziffer': 'str', 'postal_code': 'str'})
-
     # Create 'postal_code' column
-    .assign(political_district_code = lambda row: row['gemeindekennziffer'].str.slice(start=0, stop=3))
-
-    # Separate collapsed  'postal_code' column into multiple rows
-    .assign(postal_code = lambda row: row['postal_code'].str.split(pat=' ', expand=False))
-    .explode(column=['postal_code'])
-
+    .assign(
+        political_district_code=lambda row: row['gemeindekennziffer'].str.slice(
+            start=0,
+            stop=3,
+        ),
+    )
+    # Separate collapsed 'postal_code' column into multiple rows
+    .assign(
+        postal_code=lambda row: row['postal_code'].str.split(pat=' ', expand=False),
+    ).explode(column=['postal_code'])
     # Select columns
     .filter(items=['political_district_code', 'postal_code'])
-
     # Remove duplicate rows
     .drop_duplicates(subset=None, keep='first', ignore_index=True)
-
     # Left join 'at_political_districts'
-    .merge(at_political_districts, how='left', on=['political_district_code'], indicator=False)
-
+    .merge(
+        at_political_districts,
+        how='left',
+        on=['political_district_code'],
+        indicator=False,
+    )
     # Select columns
-    .filter(items=['state', 'political_district_code', 'political_district', 'postal_code'])
-
+    .filter(
+        items=['state', 'political_district_code', 'political_district', 'postal_code'],
+    )
     # Rearrange rows
     .sort_values(by=['political_district_code', 'postal_code'], ignore_index=True)
-
 )
 
 
 # Test
-(at_localities[at_localities.duplicated(subset=['postal_code'], keep=False) == True]
+(
+    at_localities[at_localities.duplicated(subset=['postal_code'], keep=False) is True]
     .reset_index(level=None, drop=True)
     .sort_values(by=['postal_code'], ignore_index=True)
 )
-
 
 
 ## Division of Austria into municipalities
 # Gliederung Österreichs in Gemeinden - Source: Statistik Austria, https://data.statistik.gv.at/web/meta.jsp?dataset=OGDEXT_GEM_1
 
 # Download
-with ZipFile(file=BytesIO(initial_bytes=requests.get(url='https://data.statistik.gv.at/data/OGDEXT_GEM_1_STATISTIK_AUSTRIA_20230101.zip').content), mode='r')) as zip_file:
+with ZipFile(
+    file=BytesIO(
+        initial_bytes=requests.get(
+            url='https://data.statistik.gv.at/data/OGDEXT_GEM_1_STATISTIK_AUSTRIA_20230101.zip',
+            timeout=5,
+        ).content,
+    ),
+    mode='r',
+) as zip_file:
     zip_file.extractall(path='OGDEXT_GEM_1_STATISTIK_AUSTRIA_20230101')
 
 
 # Import
-at_shapefile = (gpd.read_file(filename='OGDEXT_GEM_1_STATISTIK_AUSTRIA_20230101/STATISTIK_AUSTRIA_GEM_20230101.shp', layer='STATISTIK_AUSTRIA_GEM_20230101', encoding='utf8')
-
+at_shapefile = (
+    gpd.read_file(
+        filename='OGDEXT_GEM_1_STATISTIK_AUSTRIA_20230101/STATISTIK_AUSTRIA_GEM_20230101.shp',
+        layer='STATISTIK_AUSTRIA_GEM_20230101',
+        encoding='utf8',
+    )
     # Rename columns
     .rename(columns={'g_id': 'municipality_code', 'g_name': 'municipality'})
-
     # Change dtypes
     .astype(dtype={'municipality_code': 'str'})
-
     # Left join 'at_municipalities'
-    .merge(at_municipalities.filter(items=['municipality_code', 'postal_code']), how='left', on=['municipality_code'], indicator=False)
-
+    .merge(
+        at_municipalities.filter(items=['municipality_code', 'postal_code']),
+        how='left',
+        on=['municipality_code'],
+        indicator=False,
+    )
     # Left join 'at_postalcodes'
     .merge(at_postalcodes, how='left', on=['postal_code'], indicator=False)
-
     # Select columns
-    .filter(items=['country', 'state', 'municipality_code', 'municipality', 'city', 'postal_code', 'geometry'])
-
+    .filter(
+        items=[
+            'country',
+            'state',
+            'municipality_code',
+            'municipality',
+            'city',
+            'postal_code',
+            'geometry',
+        ],
+    )
     # Rearrange rows
     .sort_values(by=['country', 'postal_code'], ignore_index=True)
-
 )
 
 
@@ -216,10 +343,9 @@ at_shapefile = (gpd.read_file(filename='OGDEXT_GEM_1_STATISTIK_AUSTRIA_20230101/
 del at_municipalities, at_postalcodes
 
 
-
 # Austria Shapefile - state level (first-level administrative divisions of Austria)
-(at_shapefile
-    .filter(items=['state', 'geometry'])
+(
+    at_shapefile.filter(items=['state', 'geometry'])
     .dissolve(by='state', as_index=False, sort=True, dropna=True)
     .plot()
 )
@@ -228,8 +354,8 @@ pyplot.show()
 
 
 # Austria Shapefile - municipality level (third-level administrative divisions of Austria)
-(at_shapefile
-    .filter(items=['state', 'municipality', 'geometry'])
+(
+    at_shapefile.filter(items=['state', 'municipality', 'geometry'])
     .dissolve(by='municipality', as_index=False, sort=True, dropna=True)
     .plot()
 )
@@ -238,8 +364,8 @@ pyplot.show()
 
 
 # Austria Shapefile - postal code level
-(at_shapefile
-    .filter(items=['postal_code', 'geometry'])
+(
+    at_shapefile.filter(items=['postal_code', 'geometry'])
     .dissolve(by='postal_code', as_index=False, sort=True, dropna=True)
     .plot()
 )
